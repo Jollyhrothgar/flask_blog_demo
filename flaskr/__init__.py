cbbfe1bb64ff898afa6830932bb382d89b4cbc55
Mike Beaumier <michael.beaumier@gmail.com>
2020-05-23 13:19:01 -0700

    working on blog section

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -45,4 +45,9 @@ def create_app(test_config=None):
     from . import auth
     app.register_blueprint(auth.bp)
 
+    # Add the blog blueprint to the app
+    from . import blog
+    app.register_blueprint(blog.bp)
+    app.add_url_rule('/', endpoint='index')
+
     return app
